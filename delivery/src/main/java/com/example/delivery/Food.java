d8a3f25be1a40a026fa78cb6abdcc7c653c36f98
yeada <yeadalm04@gmail.com>
2023-11-14 06:45:11 +0900

    upadate 개별 음식 entity

--- delivery/src/main/java/com/example/delivery/Food.java
+++ delivery/src/main/java/com/example/delivery/Food.java
@@ -3,7 +3,7 @@ package com.example.delivery;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Food {
@@ -18,4 +18,43 @@ public class Food {
     private String category;
 
     // Constructors, Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void  setId(Integer id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPrice(Double price){
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
