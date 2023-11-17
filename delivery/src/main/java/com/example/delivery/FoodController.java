372c6db30864ee4dc269a9ee760de68c2c22e5b4
yeada <yeadalm04@gmail.com>
2023-11-18 06:37:31 +0900

    controller DTO <-> Entity

--- delivery/src/main/java/com/example/delivery/FoodController.java
+++ delivery/src/main/java/com/example/delivery/FoodController.java
@@ -17,35 +17,61 @@ public class FoodController {
         this.service = service;
     }
 
+    // Service 에서 DTO로 받고 Client에게 보여주기 전에 Entity
     @PostMapping
     public ResponseEntity<Food> createFood(@RequestBody Food food){
+        // DTO 로 받아서 Entity로 변환
+        Food food = convertToEntity(foodDTO);
         Food createFood = service.createAndUpdateFood(food);
-        return ResponseEntity.ok(createFood);
+        return ResponseEntity.ok(convertToDTO(createFood));
     }
 
     @GetMapping
     public ResponseEntity<Food> getFood(@PathVariable int id) {
-        return service.getFood(id)
-                .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+        //TODO  여기가 헷갈리고 이해가 잘 안된다,,,!!
+        List<Food> foods = service.getAllFoods();
+        List<FoodDTO> foodDTOs = foods.stream()
+                .map(this::converToDTO)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(foodDTOs);
     }
 
-    @GetMapping
-    public Collection<Food> getAllFoods() {
-        return service.getAllFoods();
-    }
 
     @PutMapping("/{id}")
     public ResponseEntity<Food> updateFood(@PathVariable int id, @RequestBody Food food) {
+        Food food = convertToEntity(foodDTO);
         food.setId(id);
         Food updatedFood = service.createAndUpdateFood(food);
-        return ResponseEntity.ok(updatedFood);
+        return ResponseEntity.ok(convertToDTO(updatedFood));
+        //TODO 왜 다시 반환해서 return 하지..?
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteFood(@PathVariable int id) {
         service.deleteFood(id);
         return ResponseEntity.ok().build();
+    }
+
+    // Entity -> DTO
+    private FoodDTO convertToDTO(Food food) {
+        FoodDTO dto = new FoodDTO();
+        dto.setId(food.getId());
+        dto.setName(food.getName());
+        dto.setDescription(food.getDescription());
+        dto.setPrice(food.getPrice());
+        dto.setCategory(food.getCategory());
+        return dto;
+    }
+
+    // DTO -> Entity
+    private Food convertToEntity(FoodDTO foodDTO) {
+        Food food = new Food();
+        food.setName(foodDTO.getName());
+        food.setDescription(foodDTO.getDescription());
+        food.setPrice(foodDTO.getPrice());
+        food.setCategory(foodDTO.getCategory());
+        return food;
     }
 
 }
